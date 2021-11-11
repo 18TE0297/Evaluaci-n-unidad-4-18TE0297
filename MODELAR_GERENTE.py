7f2dbdb59c0990887a7fd881c60987d42970594e
18TE0297 <L18TE0297@teziutlan.tecnm.mx>
2021-11-10 18:49:00 -0600

    Instrucciones-Cabecera

--- MODELAR_GERENTE.py
+++ MODELAR_GERENTE.py
@@ -1,5 +1,13 @@
 """
-Estos son los cambios realizados desde Pycharm
+------------------------- MODELAR GERENTE-------------------------
+Escriba una aplicación que modele en el paradigma orientado a objetos el siguiente escenario:
+“El gerente de la empresa es un empleado que mide 1.80 m, se llama <nombre> y tiene puesto un
+saco con tres botones. En este momento el gerente está entrevistando a una persona para su
+contratación.”
+
+Deberás generar múltiples archivos que representen la situación de la vida real en términos
+de programación. Así tendrás varias clases como son: gerente, persona, empleado, empresa,
+vestimenta, saco, etc. Debes establecer en ellas comportamientos como vestirse, trabajar
+y entrevistar que hagan que el escenario cobre vida.
+
 """
-print("archivo modificado desde Pycharm")
-print("nuevo mensaje ")
