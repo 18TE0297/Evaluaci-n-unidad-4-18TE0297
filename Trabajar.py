bf5b7f8dd685de97b1ec740c9ea81ad0394bcb06
18TE0297 <L18TE0297@teziutlan.tecnm.mx>
2021-11-10 21:57:49 -0600

    clases en Trabajo Gerente/Empleado

--- Trabajar.py
+++ Trabajar.py
@@ -2,3 +2,8 @@
 Clase Gerente, Nombre, Edad, Sexo, Antiguedad
 Clase Empleado, Nombre, Edad, Sexo, Area de experiencia,
 """
+import Vestirse as Persona
+class Gerente:
+    def __init__(self):
+class Empleado:
+    def __init__(self):
